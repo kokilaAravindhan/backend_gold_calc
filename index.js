ddbb8e8966ced1e20b277970d233c245682c2c38
kokilaAravindhan <koks1.kvsd@gmail.com>
2023-11-08 16:32:26 +0530

    modify

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-import express from 'express';
+
+import { Express } from 'express';
 import cors from 'cors';
 import authrouter from './routes/app-users.js';
 import connectToDb from './db-utils/mongodb_connection.js';
 
-const app = express();
+const app = Express();
 
 const PORT = process.env.PORT || 5050;
 
 // Top/Global Level await
 await connectToDb();
 
-app.use(express.static('public'));
+app.use(Express.static('public'));
 
 app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
 app.use('/api/auth', authrouter);
 app.listen(5050,()=>{console.log("successfully port is running")});
